579f32a4772043b6032afa9abcda95f67ac7bbed
EchoZhu <zykstand@foxmail.com>
2016-07-28 23:58:36 +0800

    init layout

---
+++ app/src/main/java/com/bupt/affectionfornurse/activity/UploadStatusActivity.java
@@ -0,0 +1,15 @@
+package com.bupt.affectionfornurse.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bupt.affectionfornurse.R;
+
+public class UploadStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_status);
+    }
+}
