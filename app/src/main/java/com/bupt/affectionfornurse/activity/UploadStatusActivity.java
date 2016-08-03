f2c7a3f0e0a25e360bb207c816ea3ec1157657fa
406676675@qq.com <406676675@qq.com>
2016-08-03 21:02:57 +0800

    updaate

--- app/src/main/java/com/bupt/affectionfornurse/activity/UploadStatusActivity.java
+++ app/src/main/java/com/bupt/affectionfornurse/activity/UploadStatusActivity.java
@@ -1,7 +1,9 @@
 package com.bupt.affectionfornurse.activity;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 
 import com.bupt.affectionfornurse.R;
 
@@ -11,5 +13,27 @@ public class UploadStatusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_status);
+
+       // initView();
     }
+
+    /*private void initView() {
+        //MainActivity的布局文件中的主要控件初始化
+        Toolbar mToolbar = (Toolbar) this.findViewById(R.id.tool_bar_ocr_result);
+        //初始化ToolBar
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitle("OCR结果");
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.icon_back);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UploadStatusActivity.this, UploadPhotoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }*/
 }
